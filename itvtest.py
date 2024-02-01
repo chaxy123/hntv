b0263231b9e04f96f4829ecdcd9866e6b93143fb
chaxy123 <136402151+chaxy123@users.noreply.github.com>
2024-02-01 19:35:44 +0800

    Update itvtest.py

--- itvtest.py
+++ itvtest.py
@@ -124,7 +124,7 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 file.write(f"#EXTINF:-1,tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://epg.112114.xyz/logo/{channel_name}.png" group-title="央视",{channel_name}\n{channel_url}\n")
                 channel_counters[channel_name] = 1
         channel_counters = {}
-    file.write('河南频道,#genre#\n')
+    file.write('\n')
     for result in results:
         channel_name, channel_url, speed = result
         if '河南' in channel_name:
@@ -132,10 +132,10 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"{channel_name},{channel_url}\n")
+                    file.write(f"#EXTINF:-1,tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://epg.112114.xyz/logo/{channel_name}.png" group-title="河南",{channel_name}\n{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"{channel_name},{channel_url}\n")
+                file.write(f"#EXTINF:-1,tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://epg.112114.xyz/logo/{channel_name}.png" group-title="河南",{channel_name}\n{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
     file.write('卫视频道,#genre#\n')
