cf6ca1666192603e8dfa9caaf6525b99be5c22ff
chaxy123 <136402151+chaxy123@users.noreply.github.com>
2024-02-05 13:05:23 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -7,6 +7,7 @@ import re
 
 urls = [
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",  # 河南
+    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22henan%22",    #河南
     ]
 
 def modify_urls(url):
