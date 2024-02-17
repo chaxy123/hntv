e861851c99e74e0ec42ee91e5aa7aa62047a7f2c
chaxy123 <136402151+chaxy123@users.noreply.github.com>
2024-02-17 21:31:18 +0800

    Update hnitv.py

--- hnitv.py
+++ hnitv.py
@@ -177,9 +177,9 @@ for url in urls:
         except:
             continue
 
-results.sort()
-results = set(results)  # 去重得到唯一的URL列表
 
+results = set(results)  # 去重得到唯一的URL列表
+results = sorted(results)
 with open("hnitv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
