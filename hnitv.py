6b4424528d566525d089201a6b5c343f97b72e30
chaxy123 <136402151+chaxy123@users.noreply.github.com>
2024-03-09 09:39:58 +0800

    Update hnitv.py

--- hnitv.py
+++ hnitv.py
@@ -186,6 +186,8 @@ for url in urls:
                             name = name.replace("法制", "河南法治")
                             name = name.replace("新闻", "河南新闻")
                             name = name.replace("都市", "河南都市")
+                            name = name.replace("公共", "河南公共")
+                            name = name.replace("民生", "河南民生")
                             if 'udp' not in urld or 'rtp' not in urld:
                                 results.append(f"{name},{urld}")
             except:
