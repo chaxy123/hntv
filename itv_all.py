e501e743f22ac7391957d849a5dec89008d4c149
chaxy123 <136402151+chaxy123@users.noreply.github.com>
2024-03-26 22:00:43 +0800

    Update itv_all.py

--- itv_all.py
+++ itv_all.py
@@ -379,8 +379,7 @@ with open("itvlist.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
-                
-    file.write(f"{now_today}更新,#genre#\n")
+
 with open("itvlist.m3u", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('#EXTM3U\n')
@@ -447,5 +446,3 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
-    
-    file.write(f"#EXTINF:-1 group-title=\"{now_today}更新\"\n")
